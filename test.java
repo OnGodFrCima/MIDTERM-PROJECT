cebbc0beea60200e20aa06434bb31c6411b26cd4
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2025-02-15 21:03:07 +0800

    testst
    pushing this againt o test

--- test.java
+++ test.java
@@ -1,6 +1,12 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("ON GODD");
+        System.out.println("ON GODD");
+        System.out.println("ON GODD");
+        System.out.println("ON GODD");     System.out.println("ON GODD");
+        System.out.println("ON GODD");
+        System.out.println("ON GODD");
+        
+
     }
 }
-    
