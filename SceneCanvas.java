ed7bb23a530b77493986ec9a3406d040312482b8
OwlofWis <yap.ethan040406@gmail.com>
2025-02-17 20:25:08 +0800

    Add files via upload

---
+++ SceneCanvas.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class SceneCanvas extends JComponent {
+    private ArrayList<DrawingObject>DrawingObjects;
+
+    
+    public SceneCanvas() {
+        this.setPreferredSize(new Dimension(800, 600));
+        this.DrawingObjects = new ArrayList<>();
+
+        
+       DrawingObjects.add(new Circle(100, 100, 50, Color.CYAN));  
+       DrawingObjects.add(new Circle(200, 200, 60, Color.GREEN)); 
+       DrawingObjects.add(new Rectangle(190, 250, 20, 50, Color.BROWN)); 
+       DrawingObjects.add(new Circle(500, 150, 40, Color.YELLOW)); 
+    }
+
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        
+        for (DrawingObject obj :DrawingObjects) {
+            obj.draw(g);
+        }
+    }
+
+   
+    public void updateScene() {
+        Circle cloud = (Circle)DrawingObjects.get(0);
+        cloud.x += 5;
+        if (cloud.x > this.getWidth()) {
+            cloud.x = 0;  
+        }
+
+        
+        repaint();
+    }
+}
+
+   
+      
+
+    
+      
