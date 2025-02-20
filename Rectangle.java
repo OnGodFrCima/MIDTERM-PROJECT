e871b3f9ee91741a6fd11d11dded258140d0224c
OwlofWis <yap.ethan040406@gmail.com>
2025-02-20 14:52:23 +0800

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,22 +1,32 @@
 import java.awt.*;
+import java.awt.geom.*;
 
 public class Rectangle implements DrawingObject {
 
     private double xPos, yPos, width, height;
     private Color color;
+    private double rotationAngle = 0;
 
-    public Rectangle(double xPos, double yPos, double width, double height, Color color) {
+    public Rectangle(double xPos, double yPos, double width, double height, Color color, double rotationAngle) {
         this.xPos = xPos;
         this.yPos = yPos;
         this.width = width;
         this.height = height;
         this.color = color;
+        this.rotationAngle = rotationAngle;
+         
     }
 
 
     public void draw(Graphics2D g2D) {
+
+        g2D.rotate(Math.toRadians(rotationAngle), xPos + width / 2, yPos + height / 2);
+        
+        Rectangle2D.Double rectangle = new Rectangle2D.Double(xPos, yPos, width, height);
         g2D.setColor(color);
-        g2D.fillRect((int)xPos, (int)yPos, (int)width, (int)height);  
+        g2D.fill(rectangle); 
+        
+        g2D.rotate(-Math.toRadians(rotationAngle), xPos + width / 2, yPos + height / 2);
     }
 
   
@@ -30,8 +40,12 @@ public class Rectangle implements DrawingObject {
         height += newSize;
     }
 
-    @Override
+
     public double getX() {
         return xPos;  
+    }
+
+    public void rotate(double angle) {
+        this.rotationAngle += angle;
     }
 }
