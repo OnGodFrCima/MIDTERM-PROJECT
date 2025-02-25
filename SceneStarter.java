1db19f2c7a1f31a823dab5b34c4817ce6958a8c5
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2025-02-25 18:56:53 +0800

    Update SceneStarter.java

--- SceneStarter.java
+++ SceneStarter.java
@@ -1,27 +1,8 @@
-
-import java.awt.*;
-import javax.swing.*;
-
 public class SceneStarter{
 
     public static void main(String[] args) {
-        
-        JFrame frame = new JFrame("Animated Scene");
-        SceneCanvas canvas = new SceneCanvas();
-        frame.add(canvas);
-
-        
-        JButton updateButton = new JButton("Update Scene");
-        updateButton.addActionListener(e -> canvas.updateScene());  
-
-        
-        frame.setLayout(new BorderLayout());
-        frame.add(updateButton, BorderLayout.SOUTH);  
-        frame.add(canvas, BorderLayout.CENTER);  
-
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        SceneFrame scene = new SceneFrame();
+        scene.setUpGUI();
     }
 }
 
