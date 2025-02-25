d922cf05130c02066d4b139dac5fffbbb8e0bd17
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2025-02-25 18:55:26 +0800

    Update DrawingObject.java

--- DrawingObject.java
+++ DrawingObject.java
@@ -4,7 +4,8 @@ import java.awt.*;
 public interface DrawingObject {
     void draw(Graphics2D g2d);
     void adjustX(double distance);
+    void adjustY(double distance);
     void adjustSize(double newSize);
     double getX();
-    void rotateShape(double angle); 
+    void rotate(double angle); 
 }
