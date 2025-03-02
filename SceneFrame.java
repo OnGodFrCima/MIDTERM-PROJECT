68d2fd22d9fa1bee7ab8e15be4aac9314c9a4ca6
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2025-03-03 01:14:34 +0800

    Update SceneFrame.java

--- SceneFrame.java
+++ SceneFrame.java
@@ -1,28 +1,31 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class SceneFrame extends JFrame{
+public class SceneFrame extends JFrame {
+    private SceneCanvas canvas;
+    private AudioPlayerWithButton player;
+    private AudioPlayerWithButton player2;
+    private AudioPlayerWithButton player3;
+    private AudioPlayerWithButton player4;
+    private AudioPlayerWithButton player5;
 
-    private SceneCanvas test;
+    public SceneFrame() {
+        setTitle("Midterm Project - Cimafranca - Yap.");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
 
-    public SceneFrame(){
-        setUpGUI();
+        player = new AudioPlayerWithButton("Heaven.wav"); 
+        player2 = new AudioPlayerWithButton("Ventura.wav");  
+        player3 = new AudioPlayerWithButton("Ligaya.wav");
+        player4 = new AudioPlayerWithButton("Parokya.wav");
+        player5 = new AudioPlayerWithButton("Maselang.wav");  
+        
+        canvas = new SceneCanvas(player, player2, player3, player4, player5);
+        add(canvas, BorderLayout.CENTER);
+        
+
+        pack();
+        setVisible(true);
     }
+}
 
-public void setUpGUI(){
-
-    setTitle("Midterm Project - Cimafranca - Yap.");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setLayout(new BorderLayout());  
-
-    test = new SceneCanvas();  // Initialize SceneCanvas
-    add(test, BorderLayout.CENTER);
-
-    // JButton updateButton = new JButton("Update Scene");
-    // updateButton.addActionListener(e -> test.updateScene());
-    // add(updateButton, BorderLayout.SOUTH);
-    pack();
-
-    setVisible(true);
-    }
-}   
