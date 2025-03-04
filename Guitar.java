3ea9976042952c3a170af194d6728d563e553eb5
OwlofWis <yap.ethan040406@gmail.com>
2025-03-05 01:30:31 +0800

    Update Guitar.java

--- Guitar.java
+++ Guitar.java
@@ -1,3 +1,23 @@
+/**
+    The Guitar shape is a composite shape created using multiple shapes to create the guitar. Can be seen
+    on the backgroundstage along witrh the other instruments.
+	
+	Ethan L. Yap (244883), Ethan Lawrence Cimafranca (241172)
+	March 05, 2025
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
+
 import java.awt.*;
 
 public class Guitar implements DrawingObject{
