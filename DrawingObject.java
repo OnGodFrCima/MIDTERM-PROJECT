377e70220423f947f8d0617007d2ca04c7a29dfb
OwlofWis <yap.ethan040406@gmail.com>
2025-03-05 03:08:06 +0800

    Update DrawingObject.java

--- DrawingObject.java
+++ DrawingObject.java
@@ -20,7 +20,9 @@
 
 import java.awt.*;
 
-
+/** Contains a set of methods used by all drawing objects. Void draw allows the manipulation of shapes in an object, adjustX/Y allows for the x and y values to be edited,
+similarly with adjustSize for width and height of the drawing object. getX/Y returns the x and y values of the shape
+and rotate allows said shapes to be rotated. All drawing object share these methods so I won't bother explaining this on other class files **/
 public interface DrawingObject {
     void draw(Graphics2D g2d);
     void adjustX(double distance);
