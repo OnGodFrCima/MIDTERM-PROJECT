2b38fefe0c27215f1b3eb05d541248fd64fa2430
OwlofWis <yap.ethan040406@gmail.com>
2025-03-05 01:13:15 +0800

    Update Cloud.java

--- Cloud.java
+++ Cloud.java
@@ -1,4 +1,23 @@
+/**
+	Circles galore. It's all circles. It's literally just circles grouped together to make a lumpy mess.
+    How fluffy. Voila it's a cloud, a composite shape made of exclusively circles.
+    
+	
+	Ethan L. Yap (244883), Ethan Lawrence Cimafranca (241172)
+	March 05, 2025
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
 
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
 
 
 import java.awt.*;
