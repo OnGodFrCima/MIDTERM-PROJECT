23c82242a80bd730ab7d1fdcdbac2720d4e3d945
OwlofWis <yap.ethan040406@gmail.com>
2025-03-05 01:09:04 +0800

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,3 +1,23 @@
+/**
+	The circle class. One of the most basic shapes hence why it is utilized almost everywhere. Makes up majority of
+    the composite shapes due to how versatiole it is. Is actually an ellispes with different dimensions.
+    
+	Ethan L. Yap (244883), Ethan Lawrence Cimafranca (241172)
+	March 05, 2025
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
+
 import java.awt.*;
 import java.awt.geom.*;
 
