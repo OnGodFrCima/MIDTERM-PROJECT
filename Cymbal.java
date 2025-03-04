1058287afc58ef16855e0ec42b5e422fc367a70d
OwlofWis <yap.ethan040406@gmail.com>
2025-03-05 01:18:28 +0800

    Update Cymbal.java

--- Cymbal.java
+++ Cymbal.java
@@ -1,3 +1,24 @@
+/**
+    (╯°□°）╯Drawing with lines is hard huhu. The outline is made using lines manipulated to create shapes,
+    while the color is from an ellipse that was manipulated to fit inside the outline of the shape. This took
+    a lot longer than I thought. Also a composite shape.
+	
+	Ethan L. Yap (244883), Ethan Lawrence Cimafranca (241172)
+	March 05, 2025
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
+
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 
