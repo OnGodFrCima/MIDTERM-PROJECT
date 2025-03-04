c8b370832521c4a44e515651cc9fcd1736dcacac
OnGodFrCima <ethan.lawrence.cimafranca@student.ateneo.edu>
2025-03-04 14:15:26 +0800

    Update SceneFrame.java

--- SceneFrame.java
+++ SceneFrame.java
@@ -10,7 +10,7 @@ public class SceneFrame extends JFrame {
     private AudioPlayerWithButton player5;
 
     public SceneFrame() {
-        setTitle("Midterm Project - Cimafranca - Yap.");
+        setTitle("Midterm Project-Cimafranca241172-Yap.");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
@@ -18,7 +18,7 @@ public class SceneFrame extends JFrame {
         player2 = new AudioPlayerWithButton("Ventura.wav");  
         player3 = new AudioPlayerWithButton("Ligaya.wav");
         player4 = new AudioPlayerWithButton("Parokya.wav");
-        player5 = new AudioPlayerWithButton("Maselang.wav");  
+        player5 = new AudioPlayerWithButton("ComeTogether.wav");  
         
         canvas = new SceneCanvas(player, player2, player3, player4, player5);
         add(canvas, BorderLayout.CENTER);
