1220b45d29169de20ba614625da36c228c59853c
OwlofWis <yap.ethan040406@gmail.com>
2025-03-05 02:02:32 +0800

    Update Star.java

--- Star.java
+++ Star.java
@@ -1,3 +1,22 @@
+/**
+    A compositeshape made exclusively of paths. These paths were then curved and manipulated so that
+    we could get the star shape. Can be seen in the scene change to the outside of the restaurant.
+    
+	Ethan L. Yap (244883), Ethan Lawrence Cimafranca (241172)
+	March 05, 2025
+	
+	I have not discussed the Java language code in my program 
+	with anyone other than my instructor or the teaching assistants 
+	assigned to this course.
+
+	I have not used Java language code obtained from another student, 
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program 
+	was obtained from another source, such as a textbook or website, 
+	that has been clearly noted with a proper citation in the comments 
+	of my program.
+**/
 
 
 
